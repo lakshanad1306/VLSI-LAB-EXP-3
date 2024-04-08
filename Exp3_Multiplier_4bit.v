0353ccfa21cfd05f9a09ee194e0cae588c73caf3
lakshanad1306 <161121355+lakshanad1306@users.noreply.github.com>
2024-04-08 14:14:59 +0530

    Update Exp3_Multiplier_4bit.v

--- Exp3_Multiplier_4bit.v
+++ Exp3_Multiplier_4bit.v
@@ -1,24 +1,27 @@
-module ha(a,b,sum,carry);
+module multiplier2by2(C,A,B);
+
+input [1:0]A,B;
+
+output [3:0]C;
+
+wire w1,w2,w3,w4; 
+and (C[0],A[0],B[0]); 
+and (w1,A[0],B[1]);
+and (w2,A[1],B[0]); 
+and (w3,A[1],B[1]); 
+halfadder ha1(C[1],w4,w1,w2); 
+halfadder ha2(C[2],C[3],w3,w4);
+
+endmodule
+
+module halfadder(sum,carry,a,b);
+
 input a,b;
-output sum,carry;
 
+output sum, carry;
 
-endmodule
+xor(sum,a,b);
 
-module fa_ha(a,b,c,sum,carry);
-input a,b,c;
-output sum,carry;
-
-
-endmodule
-
-module multi_4(a,b,p,carry);
-input[3:0]a,b;
-output [6:0]p;
-output carry;
-wire [17:1]w;
-
-
-
+and(carry,a,b);
 
 endmodule
